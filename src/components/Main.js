f2a07aa2481971b8d677e5fd88bcc220034b345d
Roman Brito <romanbrito1@gmail.com>
2018-01-08 10:07:46 -0600

    creating singleAsset component to show details of one Asset

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import AssetListPage from './AssetListPage'
 import CreateAsset from './CreateAsset'
 import Login from './Login'
+import SingleAsset from './SingleAsset'
 
 const Main = (props) => {
   return (
     <Switch>
       <Route exact path='/' component={AssetListPage}/>
+      <Route path='/:businessName' component={SingleAsset}/>
       <Route exact path='/create' component={CreateAsset}/>
       <Route exact path='/login' component={Login}/>
     </Switch>

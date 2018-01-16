a428bab9f232ed4c532155e6e0222dd90473b2db
Roman Brito <romanbrito1@gmail.com>
2018-01-16 10:31:24 -0600

    adding reboot in scss instad of component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Header from './Header'
 import Main from './Main'
+import '../sass/App.css' //reboot, similar to normalize.css
 import 'typeface-roboto' //roboto font
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
-       <Main/>
+        <Main/>
       </div>
     );
   }

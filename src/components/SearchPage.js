b3d4ef0e4b8ef3711c2345f596990f373eb81a2b
Roman Brito <romanbrito1@gmail.com>
2018-03-30 20:32:57 -0500

    styling search input with flexbox

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -5,17 +5,16 @@ import '../sass/SearchPage.css'
 import DisplaySearch from './DisplaySearch'
 
 const styles = theme => ({
-  search: theme.mixins.gutters({
-    marginTop: '15px',
+  search: {
+    width: '90%',
+    margin: '0 auto',
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  }),
-  hits: {
-    backgroundColor: '#fafafa'
+    flexWrap: 'wrap',
   },
   backLogo: {
-    paddingRight: 5,
+    display: 'block',
+    textAlign: 'center',
+    padding: '1% 2%'
   }
 })
 
@@ -29,23 +28,23 @@ class AlgoliaSearch extends Component {
       <InstantSearch
         appId='E4WQ29D3BH'
         apiKey='868078ad52fd8e57c099177430dd9fc3'
-        indexName='Assets'
-      >
-        <div className={classes.search}>
+        indexName='Assets'>
+
           <SearchBox
             autoFocus
             translations={{placeholder: 'Search ...'}}
             searchAsYouType={true}
           />
-          <a href="https://www.algolia.com/" target="_blank"><img className={classes.backLogo}
-                                                                  src="https://www.algolia.com/static_assets/images/pricing/pricing_new/algolia-powered-by-14773f38.svg"
-                                                                  alt=""/></a>
-        </div>
 
-        <div className={classes.hits}>
+          <a href="https://www.algolia.com/" target="_blank" className={classes.backLogo}>
+            <img src="https://www.algolia.com/static_assets/images/pricing/pricing_new/algolia-powered-by-14773f38.svg" alt=""/>
+          </a>
+
+
+
           <Hits hitComponent={hit => <DisplaySearch hit={hit} history={this.props.history}/>}/>
           <Pagination/>
-        </div>
+
 
       </InstantSearch>
     )
